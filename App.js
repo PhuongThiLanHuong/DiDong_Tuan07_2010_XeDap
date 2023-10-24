5a920b03e720d15a29bb4b052eb7f7b18562b799
LanHuong1507 <20057651.huong@student.iuh.edu.vn>
2023-10-24 14:55:37 +0700

    create Product Detail

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import HomeScreen from './Screens/HomeScreen.js'
 import ListProduct from './Screens/ListProduct.js'
+import ProductDetail from './Screens/ProductDetail.js'
 const Stack = createStackNavigator()
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} options={{headerShown:false}}/>
         <Stack.Screen name='ListProduct' component={ListProduct} options={{headerShown:false}}/>
+        <Stack.Screen name='ProductDetail' component={ProductDetail} options={{headerShown:false}}/>
       </Stack.Navigator> 
    </NavigationContainer>
   )
