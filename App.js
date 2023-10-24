8ccc2571387ff7da03dd18069bcd6f27309b23c8
LanHuong1507 <20057651.huong@student.iuh.edu.vn>
2023-10-24 14:49:04 +0700

    create List Product

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import HomeScreen from './Screens/HomeScreen.js'
+import ListProduct from './Screens/ListProduct.js'
 const Stack = createStackNavigator()
 export default function App() {
   return (
    <NavigationContainer>
      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} options={{headerShown:false}}/>
+        <Stack.Screen name='ListProduct' component={ListProduct} options={{headerShown:false}}/>
       </Stack.Navigator> 
    </NavigationContainer>
   )
